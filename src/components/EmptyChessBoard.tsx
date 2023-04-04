d83edc69058addb67f099e5be767e0ad7719872a
thelonehegelian <thelonehegelian@gmail.com>
2023-04-04 08:49:24 +0100

    Implement start-pause button

--- src/components/EmptyChessBoard.tsx
+++ src/components/EmptyChessBoard.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { BoardMap, SquareProps } from '../types';
 import { generateBoardMap } from '../constants';
 
+
 const Board = styled.div`
   display: grid;
   grid-template-columns: repeat(8, 1fr);
@@ -31,6 +32,24 @@ const Button = styled.button`
   font-size: 1.2rem;
   font-weight: bold;
   `;
+const Bar = styled.div`
+  width: 400px;
+  height: 50px;
+  background-color: #8a6d3b;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+const StartStop = styled.div`
+width: 100px;
+height: 50px;
+background-color: #8a6d3b;
+color: #f4ce7b;
+border: 1px solid #f4ce7b;
+border-radius: 5px;
+font-size: 1.2rem;
+font-weight: bold;
+`;
 
 const boardMap = generateBoardMap();
 const SquareComponent: React.FC<SquareProps> = (props) => {
@@ -48,21 +67,26 @@ const getRandomSquare = (boardMap: BoardMap): string => {
 const EmptyChessBoard: React.FC = () => {
   const squares = [];
   const [randomSquare, setRandomSquare] = React.useState(getRandomSquare(boardMap));
-  const [counter, setCounter] = React.useState(0);
+  const [counter, setCounter] = React.useState(10);
+  const [isOn, setIsOn] = React.useState(false);
     // counter display
-    React.useEffect(() => {
-      const interval = setInterval(() => {
-        setCounter(counter - 1);
-        // if counter is 0, then set the random square
-        if (counter === 0) {
-          setRandomSquare(getRandomSquare(boardMap));
-          setCounter(10);
-        }
-  
-      }, 1000);
-      return () => clearInterval(interval);
-    }, [counter]);
-  
+  React.useEffect(() => {
+    // after each 1 second, decrease counter by 1
+   if (isOn) {
+    const timer = setTimeout(() => {
+      setCounter(counter - 1);
+    }, 1000);
+  //  when the counter is 0, update the random square
+    if (counter === 0) {
+      setRandomSquare(getRandomSquare(boardMap));
+      setCounter(10);
+    }
+    return () => clearTimeout(timer);
+   }
+
+      
+
+  }, [counter, isOn]);
 
 
   const handleSquareClick = (id: string) => {
@@ -74,6 +98,11 @@ const EmptyChessBoard: React.FC = () => {
     }
   };
 
+  const handleButtonClick = (txt) => {
+    txt === 'Start'? setIsOn(true) : setIsOn(false);
+     console.log("Button clicked!", isOn);
+  }
+
 
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
@@ -83,7 +112,9 @@ const EmptyChessBoard: React.FC = () => {
     }
   }
 
-  return <><Board>{squares}{boardMap[randomSquare]}</Board><div>{counter}</div></>;
+  return <><StartStop onClick={()=> handleButtonClick(isOn ? "Pause" : "Start")}>{isOn ? "Pause" : "Start"}</StartStop> <Board>{squares}{boardMap[randomSquare]}</Board><div>{counter}</div></>;
 };
 
 export default EmptyChessBoard;
+
+
